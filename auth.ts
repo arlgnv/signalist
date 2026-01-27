bd601dd09ce283fd343ceefb0ba0b015e4b31fe5
arlgnv <arlgnv@outlook.com>
2026-01-27 13:01:24 +0300

    refactor: simplify handling baseUrl for better auth

--- auth.ts
+++ auth.ts
@@ -3,9 +3,25 @@ import { Pool } from 'pg';
 
 import environment from './environment';
 
+function getBaseUrl() {
+  if (
+    process.env.VERCEL_ENV === 'preview' &&
+    typeof process.env.VERCEL_BRANCH_URL === 'string'
+  ) {
+    return `https://${process.env.VERCEL_BRANCH_URL}`;
+  }
+
+  if (
+    process.env.VERCEL_ENV === 'production' &&
+    typeof process.env.VERCEL_PROJECT_PRODUCTION_URL === 'string'
+  ) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  }
+}
+
 const auth = betterAuth({
   appName: 'Signalist',
-  baseURL: environment.BETTER_AUTH_URL,
+  baseURL: getBaseUrl(),
   database: new Pool({
     connectionString: environment.SUPABASE_CONNECTION_STRING,
   }),
--- environment.ts
+++ environment.ts
@@ -9,37 +9,6 @@ const environment = createEnv({
     SUPABASE_PUBLISHABLE_KEY: z.string().min(1),
 
     BETTER_AUTH_SECRET: z.string().min(1),
-    BETTER_AUTH_URL: z
-      .url()
-      .optional()
-      .transform((value, ctx) => {
-        if (process.env.VERCEL_ENV === 'preview') {
-          if (typeof process.env.VERCEL_BRANCH_URL === 'undefined') {
-            ctx.issues.push({
-              input: value,
-              code: 'custom',
-              message: 'VERCEL_BRANCH_URL is required in preview environment',
-            });
-
-            return z.NEVER;
-          }
-
-          return `https://${process.env.VERCEL_BRANCH_URL}`;
-        }
-
-        if (typeof value === 'undefined') {
-          ctx.issues.push({
-            input: value,
-            code: 'invalid_type',
-            expected: 'string',
-            message: 'Invalid input: expected string, received undefined',
-          });
-
-          return z.NEVER;
-        }
-
-        return value;
-      }),
 
     GEMINI_API_KEY: z.string().min(1),
 
