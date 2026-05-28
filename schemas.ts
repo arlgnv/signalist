15ed9aeabb264b7f5eda726f1846f98280150883
arlgnv <lagunov.artamon@gmail.com>
2026-05-28 12:03:36 +0300

    refactor: make market news schema stricter

--- schemas.ts
+++ schemas.ts
@@ -2,10 +2,10 @@ import * as z from 'zod';
 
 export const MarketNewsSchema = z.object({
   category: z.string(),
-  datetime: z.number(),
+  datetime: z.int().positive(),
   headline: z.string(),
-  id: z.number(),
-  image: z.string(),
+  id: z.int().positive(),
+  image: z.url(),
   related: z.string(),
   source: z.string(),
   summary: z.string(),
