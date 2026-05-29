71c578d3847a15c161201e8631d9e69eadbd07ac
arlgnv <lagunov.artamon@gmail.com>
2026-05-29 11:30:16 +0300

    perf: ditch extra wrapper for image

--- app/(auth)/_layout/components/Image/Image.tsx
+++ app/(auth)/_layout/components/Image/Image.tsx
@@ -1,13 +1,21 @@
 'use client';
 
-import dynamic from 'next/dynamic';
+import NextImage from 'next/image';
 
-const Content = dynamic(() => import('./Content'), {
-  ssr: false,
-});
+import { useTheme } from '@/theme';
 
 function Image() {
-  return <Content />;
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <NextImage
+      className="absolute rounded-ss-2xl border-s-6 border-t-6 object-cover object-top-left"
+      src={`/images/${resolvedTheme}-home-page.jpg`}
+      alt={`Preview of the home page in ${resolvedTheme} theme`}
+      fill
+      loading="eager"
+    />
+  );
 }
 
 export default Image;
