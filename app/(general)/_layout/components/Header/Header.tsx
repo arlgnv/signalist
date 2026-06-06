eb7c81b5103a8cc75129db3bc1d4913e6f6c80ba
arlgnv <lagunov.artamon@gmail.com>
2026-06-06 20:02:53 +0300

    feat: localize base header

--- app/(general)/_layout/components/Header/Header.tsx
+++ app/(general)/_layout/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { getTranslations } from 'next-intl/server';
 import { headers } from 'next/headers';
 import Link from 'next/link';
 
@@ -11,11 +12,16 @@ async function Header() {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
+  const t = await getTranslations('header');
 
   return (
     <header className="sticky top-0 z-1 border-b bg-card text-card-foreground">
       <div className="mx-auto grid max-w-360 min-w-80 grid-cols-[1fr_auto_auto_auto] items-center gap-x-2 px-4 py-5 sm:grid-cols-[1fr_13rem_auto_auto] sm:px-8">
-        <Link className="justify-self-start" href="/" aria-label="Go home">
+        <Link
+          className="justify-self-start"
+          href="/"
+          aria-label={t('logoLinkAssistiveLabel')}
+        >
           <Logo />
         </Link>
         <Search />
@@ -26,7 +32,7 @@ async function Header() {
           <Button
             size="sm"
             nativeButton={false}
-            render={<Link href="/sign-in">Sign In</Link>}
+            render={<Link href="/sign-in">{t('signInButton.title')}</Link>}
           />
         )}
       </div>
